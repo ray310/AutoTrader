fceb7ede109bdff480840cc387b453deae574dca
ray310 <64942339+ray310@users.noreply.github.com>
2021-02-01 01:58:05 -0600

    Adding temporary client_utils to auto_trader_client/src

---
+++ auto_trader_client/src/client_utils.py
@@ -0,0 +1,18 @@
+""" General utilities """
+
+
+# TODO: this is copied from server_utils. Consolidate function to one location
+def init_root_logger(log_filename, log_directory="logs", log_level=10):
+    """Starts logger at specified location with specified formatting"""
+    import logging
+    import os
+    import time
+
+    os.makedirs(log_directory, exist_ok=True)
+    logging.Formatter.converter = time.gmtime
+    logging.basicConfig(
+        filename=os.path.join(log_directory, log_filename),
+        level=log_level,
+        format="%(levelname)s: %(message)s: %(asctime)s%(msecs)d",
+        datefmt="%m/%d/%Y %H:%M:%S",
+    )
